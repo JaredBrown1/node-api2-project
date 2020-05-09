7364fed83538e48bd97242cbee45c48f398b8e9a
Jared Brown <jaredbrown1@hotmail.com>
2020-05-08 22:50:14 -0700

    post one is working

--- posts-router.js
+++ posts-router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const shortid = require("shortid");
 const Posts = require("./data/db.js");
 
 const router = express.Router();
@@ -14,6 +15,37 @@ router.get("/", (req, res) => {
         message: "Error retrieving the posts",
       });
     });
+});
+
+router.post("/", (req, res) => {
+  const postInfo = req.body;
+  if (!postInfo.title || !postInfo.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post",
+    });
+  } else {
+    Posts.insert(postInfo)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+        res.status(500).json({
+          error: "There was an error while saving the post to the database",
+        });
+      });
+  }
+
+  // Posts.insert(req.body)
+  //   .then((post) => {
+  //     res.status(201).json(post);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     res.status(500).json({
+  //       message: "Error add the post",
+  //     });
+  //   });
 });
 
 module.exports = router;
