38df1cf1c16b200125b18e8f60e60ce09aaf1612
Jared Brown <jaredbrown1@hotmail.com>
2020-05-09 13:06:43 -0700

    PUT update is working

--- posts-router.js
+++ posts-router.js
@@ -82,10 +82,11 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+//DELETE a post by specified id
 router.delete("/:id", (req, res) => {
   Posts.remove(req.params.id)
-    .then((count) => {
-      if (count > 0) {
+    .then((post) => {
+      if (post > 0) {
         res.status(200).json({ message: "The post has been deleted" });
       } else {
         res.status(404).json({ message: "The post could not be found" });
@@ -95,6 +96,25 @@ router.delete("/:id", (req, res) => {
       console.log(error);
       res.status(500).json({
         message: "Error removing the post",
+      });
+    });
+});
+
+//PUT updates a current post
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the post",
       });
     });
 });
